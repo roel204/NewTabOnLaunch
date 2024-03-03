0e15cf923c2dafe2baf368a9c17466becb78ff4c
roelh <roelhoogendoorn01369@gmail.com>
2024-03-03 20:04:00 +0100

    Tried to add auto minimize tab groups. But not supported on current chrome version.

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
-chrome.runtime.onStartup.addListener(function() {
-    chrome.tabs.query({ title: "New Tab" }, function(tabs) {
-        tabs.forEach(tab => chrome.tabs.remove(tab.id));
+chrome.runtime.onStartup.addListener(function () {
+    // Check for existing "New Tab" and close them
+    chrome.tabs.query({title: "New Tab"}, function (tabs) {
+        tabs.forEach(tab =>
+            chrome.tabs.remove(tab.id)
+        );
     });
-    chrome.tabs.create({ url: "chrome://newtab", active: true });
+
+    // Minimize all tab groups (Currently not working, chrome fix is in progress)
+    // chrome.windows.getCurrent(function (win) {
+    //     chrome.tabGroups.query({ windowId: win.id }, function (groups) {
+    //         groups.forEach(group =>
+    //             chrome.tabGroups.update(group.id, {collapsed: true})
+    //         );
+    //     })
+    // })
+
+    // Create the new tab (existing code)
+    chrome.tabs.create({url: "chrome://newtab", active: true});
 });
