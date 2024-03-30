5de1a3f71611a497d573430bc75b03842dcc13cb
Roel <roelhoogendoorn01369@gmail.com>
2024-03-30 17:44:06 +0100

    Added ensureNewTab function

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
 chrome.runtime.onStartup.addListener(function () {
-    // Check for existing "New Tab" and close them
-    chrome.tabs.query({title: "New Tab"}, function (tabs) {
-        tabs.forEach(tab =>
-            chrome.tabs.remove(tab.id)
-        );
-    });
 
-    // Minimize all tab groups (Currently not working, chrome fix is in progress)
+    ensureNewTab()
+
+    // Minimize all tab groups
     // chrome.windows.getCurrent(function (win) {
     //     chrome.tabGroups.query({ windowId: win.id }, function (groups) {
     //         groups.forEach(group =>
@@ -14,7 +10,50 @@ chrome.runtime.onStartup.addListener(function () {
     //         );
     //     })
     // })
-
-    // Create the new tab (existing code)
-    chrome.tabs.create({url: "chrome://newtab", active: true});
 });
+
+chrome.tabs.onActivated.addListener(function () {
+    ensureNewTab()
+})
+
+function createNewTab() {
+    // Check for existing "New Tab" and close them
+    chrome.tabs.query({title: "New Tab"}, function (tabs) {
+        tabs.forEach(tab =>
+            chrome.tabs.remove(tab.id)
+        );
+    });
+
+    // Create new tab
+    chrome.tabs.create({url: "chrome://newtab", active: true});
+}
+
+// Function to always have a new tab open
+async function ensureNewTab() {
+    // Get all existing tabs
+    const tabs = await chrome.tabs.query({});
+
+    // Find all "New Tab" tabs
+    const newTabsIndex = tabs.map((tab, index) => tab.title === "New Tab" ? index : null).filter(index => index !== null);
+
+    // Keep only the last "New Tab" and close any extra ones
+    if (newTabsIndex.length > 1) {
+        for (const tabIndex of newTabsIndex.slice(0, -1)) {
+            setTimeout(() => {
+                chrome.tabs.remove(tabs[tabIndex].id);
+            }, 100);
+        }
+    }
+
+    // Move the last remaining "New Tab" to the desired position (or create one if none exist)
+    const newTabIndex = newTabsIndex.length > 0 ? newTabsIndex[0] : -1;
+    if (newTabIndex !== -1 && newTabIndex !== tabs.length - 1) {
+        setTimeout(() => {
+            chrome.tabs.move(tabs[newTabIndex].id, {index: tabs.length});
+        }, 100);
+    } else if (!~newTabIndex) {
+        setTimeout(() => {
+            chrome.tabs.create({url: "chrome://newtab", active: false, index: tabs.length});
+        }, 100);
+    }
+}
