4a2f80f36b53e80b8aa32ecf156edcc2179c5bc7
Roel <roelhoogendoorn01369@gmail.com>
2024-11-26 20:02:02 +0100

    Adds options and maybe some other stuff

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
-chrome.runtime.onStartup.addListener(function () {
+let newTabOnLaunchOption = false;
+let ensureNewTabOption = false;
 
-    ensureNewTab(true)
+// Get all options from the sync storage
+async function setOptions() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(['newTabOnLaunch', 'ensureNewTab'], function (data) {
+            // Use the retrieved options here
+            newTabOnLaunchOption = data.newTabOnLaunch || false;
+            ensureNewTabOption = data.ensureNewTab || false;
+            console.log(data)
+            resolve()
+        })
+    })
+}
+
+// Code that runs on startup
+chrome.runtime.onStartup.addListener(async function () {
+
+    await setOptions()
+
+    console.log(newTabOnLaunchOption)
+    // Create a new active tab on launch
+    if (newTabOnLaunchOption) {
+        ensureNewTab(true)
+    }
 
     // Minimize all tab groups (Not working currently, waiting on chrome update)
     // chrome.windows.getCurrent(function (win) {
@@ -33,7 +56,10 @@ chrome.tabs.onRemoved.addListener(function () {
 
 // Function that runs when there are changes to the tabs
 function tabChange() {
-    ensureNewTab(false)
+    // Ensure a new tab is always open
+    if (ensureNewTabOption) {
+        ensureNewTab(false)
+    }
 }
 
 // Function to always have a new tab open
@@ -57,13 +83,17 @@ async function ensureNewTab(isActive) {
     if (newTabIndex !== -1 && newTabIndex !== tabs.length - 1) {
         chrome.tabs.move(tabs[newTabIndex].id, {index: tabs.length});
 
+        // Check if the moved tab needs to be active
+        if (isActive) {
+            chrome.tabs.update(tabs[newTabIndex].id, {active: true});
+        }
+
         // If there is no new tab, create one
     } else if (!~newTabIndex) {
         chrome.tabs.create({url: "chrome://newtab", active: isActive, index: tabs.length});
-    }
 
-    // Check if the "New Tab" needs to be active
-    if (isActive) {
+        // If a "New Tab" exists and doesn't need to be moved, check if it needs to be active
+    } else if (isActive) {
         chrome.tabs.update(tabs[newTabIndex].id, {active: true});
     }
 }
